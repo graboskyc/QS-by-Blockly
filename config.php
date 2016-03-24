4f203807ba086f7f15cb20dbf39ba49d77de81f5
graboskyc <chris@grabosky.net>
2016-03-24 14:49:28 -0400

    Fixed bug in Env Pkg since config was using absolute path. Now using relative path so it works standard deploy or as the service

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
 // CONFIGURATION
 //////////////////////////
 $QS_PathToCSPython = 'C:\Program Files (x86)\QualiSystems\TestShell\ExecutionServer\python\2.7.10\python.exe';
-$QS_PathToGetDetails = 'C:\inetpub\wwwroot\getDetails.py';
-$QS_PathToUploadPkg = 'C:\inetpub\wwwroot\uploadPackage.py';
+$QS_PathToGetDetails = getcwd().'\getDetails.py';
+$QS_PathToUploadPkg = getcwd().'\uploadPackage.py';
 ?>
